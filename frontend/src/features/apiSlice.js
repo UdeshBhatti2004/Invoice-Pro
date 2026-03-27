82c137b93f72af090ef6ff144f0d2f51cc56ee49
Udesh Bhatti <udeshbhatti@gmail.com>
2026-03-27 13:13:59 +0530

    added vite url in  frontend for vercel deployment

--- frontend/src/features/apiSlice.js
+++ frontend/src/features/apiSlice.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080",
+    baseUrl: import.meta.env.VITE_API_URL, 
+    credentials: "include", 
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth?.token;
       if (token) headers.set("Authorization", `Bearer ${token}`);

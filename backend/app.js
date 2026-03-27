d6ba85cb3fd405b4af92d8e54bb8f41926721d36
Udesh Bhatti <udeshbhatti@gmail.com>
2026-03-27 12:22:26 +0530

    fixed app.js for uptime robot

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
-import dns from 'dns';
-dns.setDefaultResultOrder('ipv4first');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
-
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 import express from "express";
 import dotenv from "dotenv";
@@ -12,35 +11,48 @@ import authRoutes from "./routes/AuthRoutes.js";
 import invoiceRoutes from "./routes/InvoiceRoutes.js";
 import dashboardRoutes from "./routes/DashboardRoutes.js";
 import aiRoutes from "./routes/AiRoutes.js";
-import clientRoute from "./routes/ClientRoute.js"
+import clientRoute from "./routes/ClientRoute.js";
 import "./worker/reminderWorker.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
+
 
 connectDB();
 
-if (redisClient.isOpen) {
-  console.log(" Redis is ready to use");
-}
+redisClient.on("connect", () => {
+  console.log("Redis connected");
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
 
 app.use(express.json());
+
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
   })
 );
+
+// Ping route (for UptimeRobot)
+app.get("/ping", (req, res) => {
+  res.send("OK");
+});
+
 
 app.use("/auth", authRoutes);
 app.use("/invoice", invoiceRoutes);
 app.use("/dashboard", dashboardRoutes);
 app.use("/ai", aiRoutes);
 app.use("/clients", clientRoute);
+
+
 app.listen(PORT, () => {
-  console.log(` Server running on PORT ${PORT}`);
+  console.log(`Server running on PORT ${PORT}`);
 });

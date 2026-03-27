2d13710ba19ccda373a4b3324e0b13457e35e2d3
Udesh Bhatti <udeshbhatti@gmail.com>
2026-03-27 13:36:24 +0530

    fixed smtp transporter

--- backend/controllers/AiControllers.js
+++ backend/controllers/AiControllers.js
@@ -11,7 +11,9 @@ dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
